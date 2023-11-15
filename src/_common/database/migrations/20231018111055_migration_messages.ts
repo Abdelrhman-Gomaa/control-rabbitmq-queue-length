32a76cd8e5e24e28e170caa33eff5a813be404a1
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-11-15 11:44:47 +0200

    feat: database configuration

---
+++ src/_common/database/migrations/20231018111055_migration_messages.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	if (!await knex.schema.hasTable('messages'))
+		return knex.schema
+			.createTable('messages', table => {
+				table.increments('id').primary();
+				table.string('to').notNullable();
+				table.string('from').notNullable();
+				table.string('content').notNullable();
+				table.enu('type', ['pending', 'success', 'failed']);
+				table.timestamp('resolvedAt').nullable();
+				table.timestamps(true, true);
+			});
+}
+
+export async function down(knex: Knex): Promise<void> { }
